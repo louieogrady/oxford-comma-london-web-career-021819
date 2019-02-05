5e69a6982596a497a42784c5125dbfa151fb5b9d
Louie O'Grady <l.ogrady@gmail.com>
2019-02-05 10:05:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,7 @@ def oxford_comma(array)
     array.join(' and ')
     elsif array.length == 3
     array.insert(2, 'and')
-    output = array.join(', ')    
-    output[19] = ''
+    array.join(', ')    
     end
 end
 
