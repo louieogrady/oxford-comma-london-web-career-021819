708053272170cd9ca163f779f36d3dc77a715bdf
Louie O'Grady <l.ogrady@gmail.com>
2019-02-05 10:18:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if
+    array.length < 2
+    array.join
+    elsif array.length == 2
+    array.join(' and ')
+    elsif array.length == 3
+    array.map {|x| x << ", " }
+    array.insert(-2, 'and ')
+    string_output = array.join
+    string_output.chop.chop
+    elsif array.length > 3
+    array.map {|x| x << ", " }
+    array.insert(-2, 'and ')
+    string_output = array.join
+    string_output.chop.chop
+    end
 end
