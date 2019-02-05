37934cb25fb98d882cbf0cbadc6a3217b5362932
Louie O'Grady <l.ogrady@gmail.com>
2019-02-05 09:44:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  array.map {|x| x << ","}
+  output = array.join(" ")
+  
+  return output 
 end
+
+
+
+#def oxford_comma(array)
+#  array.map {|x| x << ","}
+#  output = array.join(" ")
+#  output.insert 18, " and"
+#  output[31] = ''
+#  return output 
+#end
+
+
+
