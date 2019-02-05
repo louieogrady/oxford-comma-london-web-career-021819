6e115b2b11af8311c73a12438d8b79ba0b9d9ee6
Louie O'Grady <l.ogrady@gmail.com>
2019-02-05 09:56:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ def oxford_comma(array)
     elsif array.length == 2
     array.join(' and ')
     elsif array.length == 3
-    array.map {|x| x << ","}
-    array.insert(2, ' and ')
+    array.map {|x| x << ", "}
+    array.insert(2, 'and ')
     array.join
     end
 end
